752bfb72eeb67d7c5ad9d3d61681f6714e25e1e2
Jacob <104524307+Jacob-1122@users.noreply.github.com>
2025-03-12 20:11:59 -0500

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  base: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    },
+    assetsDir: 'assets',
+    sourcemap: true,
+  },
+  server: {
+    port: 3000
+  }
+});
